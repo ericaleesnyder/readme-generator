9f358a34c24329941619a194bf6aa58a47e87a54
Erica Snyder <ericaleesnyder@gmail.com>
2021-10-14 16:43:48 -0700

    sample readme generated, test input logged

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const questions = [
   {
     name: "title",
     type: "input",
-    message: "What is the name of your application?"
+    message: "What is the name of your application's github repository?"
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "Please write a brief description of your file"
   },
   {
     name: "installation",
@@ -38,13 +43,78 @@ const questions = [
     message: "What is license of your application?",
     choices: ["a", "b", "c"]
   },
+  {
+    name: "contributing",
+    type: "input",
+    message: "Please list any contributors",
+  },
+  {
+    name: "tests",
+    type: "input",
+    message: "Are there any tests that can be performed?",
+  },
+  {
+    name: "github",
+    type: "input",
+    message: "What is your github username?",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is your email?",
+  },
 ];
-inquirer.prompt(questions)
-.then((answers) => console.log(answers))
 
-// TODO: Create a function to write README file
-// use template literals and variables based on user input (refer to miniproject)
-function writeToFile(fileName, data) {}
+inquirer.prompt(questions)
+.then((answers) => {
+  console.log(answers);
+  //
+  const fileName = "./output/README.md";
+  // COPY IN CONTENT FROM SAMPLE README FILE INTO FILECONTENT, REPLACE PLACEHOLDERS WITH ANSWERS.NAMES
+  const fileContent = 
+  `# ${answers.title}
+
+### Description
+
+${answers.description}
+
+The deployed version of the application can be found at:
+https://ericaleesnyder.github.io/${answers.title}/
+
+## Table of Contents
+1. Installation
+2. Useage
+3. License
+4. Contributing
+5. Tests
+6. Questions
+
+### Useage
+
+${answers.useage}
+
+### License
+
+The license for this application is ${answers.license}
+
+### Contributing
+
+Other contributors to this application are:
+${answers.contributing}
+
+### Tests
+
+You can run the following tests to ensure the application is functioning properly:
+${answers.tests}
+
+### Questions
+
+If you have any questions about this application, my github profile is ${answers.github}, and you can email me at ${answers.email}.
+`;
+
+  fs.writeFile(fileName, fileContent, (err) =>
+  err ? console.error(err) : console.log(`Readme saved as ${fileName}`))
+})
 
 // TODO: Create a function to initialize app
 function init() {}
---
+++ output/README.md
@@ -0,0 +1,38 @@
+# test
+
+### Description
+
+test
+
+The deployed version of the application can be found at:
+https://ericaleesnyder.github.io/undefined/
+
+## Table of Contents
+1. Installation
+2. Useage
+3. License
+4. Contributing
+5. Tests
+6. Questions
+
+### Useage
+
+test
+
+### License
+
+The license for this application is b
+
+### Contributing
+
+Other contributors to this application are:
+test
+
+### Tests
+
+You can run the following tests to ensure the application is functioning properly:
+test
+
+### Questions
+
+If you have any questions about this application, my github profile is test, and you can email me at test.
